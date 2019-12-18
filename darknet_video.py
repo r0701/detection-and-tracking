fec401c95c0f1e28559b84fa3da0cdaa14f95cff
Lambda <20190701@ku.ac.ae>
2019-12-18 14:18:11 +0400

    detection processing

--- darknet_video.py
+++ darknet_video.py
@@ -9,18 +9,12 @@ import darknet
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--h', type=str, default='short_test.mp4',
-                    help='use --cfg --weights --data --source')
-parser.add_argument('--cfg', type=str,
-                    default='short_test.mp4', help='path to cfg')
-parser.add_argument('--source', type=str,
-                    default='short_test.mp4', help='path to video')
-parser.add_argument('--weight', type=str,
-                    default='short_test.mp4', help='path to weight')
-parser.add_argument('--data', type=str,
-                    default='short_test.mp4', help='path to data file')
+parser.add_argument('--h', type=str, default='short_test.mp4', help='use --cfg --weights --data --source')
+parser.add_argument('--cfg', type=str, default='short_test.mp4', help='path to cfg')
+parser.add_argument('--source', type=str, default='short_test.mp4', help='path to video')
+parser.add_argument('--weight', type=str, default='short_test.mp4', help='path to weight')
+parser.add_argument('--data', type=str, default='short_test.mp4', help='path to data file')
 opt = parser.parse_args()
-
 
 def convertBack(x, y, w, h):
     xmin = int(round(x - (w / 2)))
@@ -31,7 +25,6 @@ def convertBack(x, y, w, h):
 
 
 def cvDrawBoxes(detections, img):
-    middle = 0
     for detection in detections:
         x, y, w, h = detection[2][0],\
             detection[2][1],\
@@ -41,15 +34,15 @@ def cvDrawBoxes(detections, img):
             float(x), float(y), float(w), float(h))
         pt1 = (xmin, ymin)
         pt2 = (xmax, ymax)
-
+        
         cv2.rectangle(img, pt1, pt2, (0, 255, 0), 1)
         cv2.putText(img,
                     detection[0].decode() +
                     " [" + str(round(detection[1] * 100, 2)) + "]",
                     (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                     [0, 255, 0], 2)
-        middle = ((pt1[0]+pt2[0])/2, (pt1[1]+pt2[1])/2)
-    return img, middle
+                    
+    return img
 
 
 netMain = None
@@ -110,12 +103,10 @@ def YOLO():
 
     # Create an image we reuse for each detect
     darknet_image = darknet.make_image(darknet.network_width(netMain),
-                                       darknet.network_height(netMain), 3)
+                                    darknet.network_height(netMain),3)
 
     first_detection = 0
     frame_number = 0
-    detection_frame = 0
-    magnitude = 0
     while True:
         prev_time = time.time()
         ret, frame_read = cap.read()
@@ -125,23 +116,19 @@ def YOLO():
                                     darknet.network_height(netMain)),
                                    interpolation=cv2.INTER_LINEAR)
 
-        darknet.copy_image_from_bytes(darknet_image, frame_resized.tobytes())
+        darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
 
-        if magnitude is 0 or magnitude > 20:
-            detections = darknet.detect_image(
-                netMain, metaMain, darknet_image, thresh=0.25)
-
+        detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
+        
         if first_detection is 1:
             success, boxes = multiTracker.update(frame_resized)
 
             for i, newbox in enumerate(boxes):
                 p1 = (int(newbox[0]), int(newbox[1]))
                 p2 = (int(newbox[0] + newbox[2]), int(newbox[1] + newbox[3]))
-                cv2.rectangle(frame_resized, p1, p2, (0, 0, 255), 2, 1)
+                cv2.rectangle(frame_resized, p1, p2, (0,0,255), 2, 1)
 
         # if something is detected
-        if len(detections) is not 0:
-            detection_frame += 1
         if len(detections) is not 0 and first_detection is 0:
             print(detections[0])
             x, y, w, h = detections[0][2][0],\
@@ -149,40 +136,29 @@ def YOLO():
                 detections[0][2][2],\
                 detections[0][2][3]
             print(f'tracker box is: {x} {y} {w} {h}')
-            multiTracker.add(cv2.TrackerCSRT_create(),
-                             frame_resized, (x-w/2, y-h/2, w, h))
+            multiTracker.add(cv2.TrackerCSRT_create(), frame_resized, (x-w/2,y-h/2,w,h))
             #multiTracker.add(cv2.TrackerMIL_create(), image, (x,y,w+5,h+5))
             first_detection = 1
 
-        separate = False
+        
 
-        image, det_mid = cvDrawBoxes(detections, frame_resized)
-        try:
-            if det_mid is not 0:
-                track_mid = ((p1[0]+p2[0])/2, (p1[1]+p2[1])/2)
-                print(f'middle of detection is {det_mid}')
-                print(f'middle of tracker is {track_mid}')
-                magnitude = int(
-                    math.sqrt((det_mid[1]-track_mid[1])**2+(det_mid[0]-track_mid[0])**2))
-                print(magnitude)
-                separate = True
-        except:
-            pass
+            
+
+        
+        
+        #image = cvDrawBoxes(detections, frame_resized)
         print(1/(time.time()-prev_time))
         #image = cv2.resize(image,(1920, 1080))
-        frame_resized = cv2.cvtColor(frame_resized, cv2.COLOR_RGB2BGR)
+        frame_resized=cv2.cvtColor(frame_resized, cv2.COLOR_RGB2BGR)
         cv2.imshow('Demo', frame_resized)
         cv2.waitKey(3)
-        print(f'[FRAME] detected frames {detection_frame}/{frame_number}')
-        if magnitude > 20:
+        if frame_number % 50 ==0:
             multiTracker = cv2.MultiTracker_create()
             first_detection = 0
-            separate = False
 
-        frame_number += 1
+        frame_number+=1
     cap.release()
     out.release()
-
 
 if __name__ == "__main__":
     YOLO()
